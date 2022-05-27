e280d36d04411f7f602b39f662db3c9f3b05a5dd
kimhyeji <gpwl5589@naver.com>
2022-05-27 20:34:10 +0900

    게시물 작성시 로그인 된 회원이 작성자가 됨

--- src/com/kor/java/ssg/Controller/Controller.java
+++ src/com/kor/java/ssg/Controller/Controller.java
@@ -1,7 +1,14 @@
 package com.kor.java.ssg.Controller;
 
+import com.kor.java.ssg.dto.Member;
+
 public abstract class Controller {
+	public static Member loginedMember;
 	public abstract void doAction(String command, String actionMethodName);
 	
 	public abstract void makeTestData();
+	
+	public static boolean isLogined() {
+		return loginedMember != null;
+	}
 }
