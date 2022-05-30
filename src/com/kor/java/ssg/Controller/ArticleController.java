ff6d894122f67f6d02cc7b2d22fd3effd49bd125
kimhyeji <gpwl5589@naver.com>
2022-05-30 19:21:52 +0900

    작성자만 게시물 수정/삭제 가능

--- src/com/kor/java/ssg/Controller/ArticleController.java
+++ src/com/kor/java/ssg/Controller/ArticleController.java
@@ -145,6 +145,11 @@ public class ArticleController extends Controller {
 			System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 			return;
 		}
+		
+		if (foundArticle.memberId != loginedMember.id) {
+			System.out.printf("권한이 없습니다.\n");
+			return;
+		}
 
 		System.out.printf("제목 : ");
 		String title = sc.nextLine();
@@ -173,14 +178,19 @@ public class ArticleController extends Controller {
 		String[] commandBits = command.split(" ");
 		int id = Integer.parseInt(commandBits[2]);
 
-		int foundIndex = getArticleIndexById(id);
+		Article foundArticle = getArticleById(id);
 
-		if (foundIndex == -1) {
+		if (foundArticle == null) {
 			System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 			return;
 		}
+		
+		if (foundArticle.memberId != loginedMember.id) {
+			System.out.printf("권한이 없습니다.\n");
+			return;
+		}
 
-		articles.remove(foundIndex);
+		articles.remove(foundArticle);
 		System.out.printf("%d번 게시물이 삭제되었습니다.\n", id);
 	}
 
