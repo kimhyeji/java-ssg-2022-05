e1876d1908cc07eb48844958e3c48d47c83e8288
kimhyeji <gpwl5589@naver.com>
2022-06-01 19:31:05 +0900

    회원 서비스 도입

--- src/com/kor/java/ssg/container/Container.java
+++ src/com/kor/java/ssg/container/Container.java
@@ -3,15 +3,18 @@ package com.kor.java.ssg.container;
 import com.kor.java.ssg.dao.ArticleDao;
 import com.kor.java.ssg.dao.MemberDao;
 import com.kor.java.ssg.service.ArticleService;
+import com.kor.java.ssg.service.MemberService;
 
 public class Container {
 	public static ArticleDao articleDao;
 	public static MemberDao memberDao;
 	public static ArticleService articleService;
+	public static MemberService memberService;
 	
 	static {
 		articleDao = new ArticleDao();
 		memberDao = new MemberDao();
 		articleService = new ArticleService();
+		memberService = new MemberService();
 	}
 }
---
+++ src/com/kor/java/ssg/service/MemberService.java
@@ -0,0 +1,26 @@
+package com.kor.java.ssg.service;
+
+import com.kor.java.ssg.container.Container;
+import com.kor.java.ssg.dao.MemberDao;
+import com.kor.java.ssg.dto.Member;
+
+public class MemberService {
+	private MemberDao memberDao;
+	
+	public MemberService() {
+		memberDao = Container.memberDao;
+	}
+
+	public Member getMemberByLoginId(String loginId) {
+		return memberDao.getMemberByLoginId(loginId);
+	}
+
+	public int getMemberIndexByLoginId(String loginId) {
+		return memberDao.getMemberIndexByLoginId(loginId);
+	}
+
+	public void join(Member member) {
+		memberDao.add(member);
+	}
+
+}
